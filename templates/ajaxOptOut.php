33ecd3ca5d8f80b51ae25aba16b686d91a065425
Tim J. Peters <tim_peters@gmx.de>
2018-09-22 15:12:05 +0200

    Rewrote jquery parts to vanilla js

--- templates/ajaxOptOut.php
+++ templates/ajaxOptOut.php
@@ -4,7 +4,6 @@
     <input type="checkbox" id="piwikOptOutCheckbox">
     <span id="piwikOptOutText"></span>
 </label>
-
 <script>
-    window.onload=function(){function e(a){1==a?b.html(c):b.html(d)}function f(a,b){$.ajax({url:"<?= $url; ?>/index.php?module=API&method=AjaxOptOut."+a+"&format=json",jsonp:"callback",dataType:"jsonp",contentType:"application/json",success:b,error:function(a){console.log(a)}})}var a=$("#piwikOptOutCheckbox"),b=$("#piwikOptOutText"),c="<?= l::get('ka.piwik.trackOn'); ?>",d="<?= l::get('ka.piwik.trackOff'); ?>";f("isTracked",function(b){e(b.value),a.prop("checked",b.value)}),a.click(function(){f(1==this.checked?"doTrack":"doIgnore"),e(this.checked)})};
+    window.onload=function(){var e=document.querySelector("#piwikOptOutCheckbox"),o=document.querySelector("#piwikOptOutText"),t="<?= l::get('ka.piwik.trackOn'); ?>",n="<?= l::get('ka.piwik.trackOff'); ?>";function c(e){o.innerHTML=1==e?t:n}function a(e,o){var t="<?= $url; ?>/index.php?module=API&method=AjaxOptOut."+e+"&format=json",n=Math.floor(65536*(1+Math.random())).toString(16).substring(1);var c="_jsonp_"+n;t.match(/\?/)?t+="&callback="+c:t+="?callback="+c;var a=document.createElement("script");a.type="text/javascript",a.src=t,window[c]=function(e){void 0!=o&&o.call(window,e),document.getElementsByTagName("head")[0].removeChild(a),delete window[c]},document.getElementsByTagName("head")[0].appendChild(a)}a("isTracked",function(o){c(o.value),e.checked=o.value}),e.onclick=function(){1==this.checked?a("doTrack"):a("doIgnore"),c(this.checked)}};
 </script>
